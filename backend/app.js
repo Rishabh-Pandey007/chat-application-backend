feb131564f348c27085d56129ceb37999aa529c6
Rishabh-Pandey007 <rishabhpandey2023@gmail.com>
2024-07-20 23:22:27 +0530

    basic setup of project

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,36 @@
-const path = require("path");
+import express from "express";
+import dotenv from "dotenv"; 
 
-const express = require("express");
+
+import connectDB from "./utils/dbConnection.js";
+
+
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+dotenv.config({});
+
+ 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
-const fileUpload = require('express-fileupload');
 
-require("./env");
+// middleware
+app.use(express.urlencoded({extended:true}));
+app.use(express.json()); 
+app.use(cookieParser());
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+const corsOption={
+    origin:`http://localhost:${PORT}`,
+    credentials:true
+};
+app.use(cors(corsOption)); 
 
-app.use(express.static('public'));
-
-const cors = require("cors");
-app.use(cors());
-app.use(fileUpload());
+// connect to database
+connectDB();
 
 
+app.listen(PORT, ()=>{
+    console.log(`Server listen at prot ${PORT}`);
+});
 
-const PORT = process.env.PORT || 5001;
-app.listen(PORT);
-console.log("Running on port = ", PORT);
