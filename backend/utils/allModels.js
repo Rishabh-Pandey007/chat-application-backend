775264601dae09ee2c2fabc73d7c6576df86c7ae
Rishabh-Pandey007 <rishabhpandey2023@gmail.com>
2024-08-17 12:46:48 +0530

    backend models in progress

---
+++ backend/utils/allModels.js
@@ -0,0 +1,11 @@
+
+
+const User = require('./user');
+const Message = require('./message');
+const Conversation = require('./conversation');
+
+module.exports = {
+    User,
+    Message,
+    Conversation
+}
